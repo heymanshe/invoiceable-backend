ed0145f575ccd6a94b37c1b931f834c9dd1564ae
heymanshe <himanshiksharma@outlook.com>
2025-03-06 15:31:54 +0530

    updated invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,2 +1,14 @@
 class Invoice < ApplicationRecord
+  validates :invoice_number, presence: true, uniqueness: true
+  validates :date, :due_date, presence: true
+  validates :invoice_type, presence: true
+  validates :total_amount, numericality: { greater_than_or_equal_to: 0 }
+
+  before_save :calculate_total_amount
+
+  private
+
+  def calculate_total_amount
+    self.total_amount = items.sum(&:amount)
+  end
 end
