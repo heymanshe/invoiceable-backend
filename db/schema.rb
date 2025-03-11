ed2e1938657c422e2627d28495eb0ebafe2b2777
heymanshe <himanshiksharma@outlook.com>
2025-03-11 11:00:35 +0530

    generated industry schema for invoice template

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_06_101832) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_10_153918) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "industries", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_industries_on_name", unique: true
+  end
 
   create_table "invoices", force: :cascade do |t|
     t.string "invoice_number", null: false
