a69c878a058f77b530312a8e4f09e1bb8cc8e83e
heymanshe <himanshiksharma@outlook.com>
2025-03-11 16:51:53 +0530

    updated UserMailer to accept user instance for welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   default from: "no-reply@example.com"
 
-  def welcome_email
+  def welcome_email(user)
     @user = user
     @url  = "http://localhost:3000/login"
     mail(to: @user.email, subject: "Welcome to Invoiceable")
