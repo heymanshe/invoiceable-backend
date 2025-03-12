58fe55398ba00a1d73ee43da3524fe455d5c976f
heymanshe <himanshiksharma@outlook.com>
2025-03-12 13:01:41 +0530

    configured CORS issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session, if: -> { request.format.json? }
+  # protect_from_forgery with: :null_session, if: -> { request.format.json? }
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
