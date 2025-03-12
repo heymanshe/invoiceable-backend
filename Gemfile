18627164ac52b3961cc6f749bd331d8e3367485d
heymanshe <himanshiksharma@outlook.com>
2025-03-12 11:44:33 +0530

    hot fixes

--- Gemfile
+++ Gemfile
@@ -40,6 +40,16 @@ gem "thruster", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+
+gem "bcrypt", "~> 3.1.7"
+
+# api auth
+gem "jwt"
+
+
+# Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin Ajax possible
+gem "rack-cors"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
@@ -49,6 +59,9 @@ group :development, :test do
 
   # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
   gem "rubocop-rails-omakase", require: false
+
+  # catch development mails
+  gem "letter_opener"
 end
 
 group :development do
---
+++ app/controllers/api/invoices_controller.rb
@@ -0,0 +1,40 @@
+class Api::InvoicesController < ApplicationController
+  def index
+    invoices = Invoice.all
+    render json: invoices
+  end
+
+  def create
+    ActiveRecord::Base.transaction do
+      invoice = Invoice.new(invoice_params)
+
+      invoice.raw_data = params.require(:invoice).permit(
+        :invoice_number, :date, :due_date, :notes, :template, :invoice_type, :template_config,
+        from: [ :name, :email, :address ],
+        to: [ :name, :email, :address ],
+        items: [ :id, :quantity, :price, :amount, :description ]
+      ).to_h
+
+      if invoice.save
+        render json: invoice, status: :created
+      else
+        render json: invoice.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { error: e.message }, status: :unprocessable_entity
+  rescue StandardError => e
+    render json: { error: e.message }, status: :internal_server_error
+  end
+
+  private
+
+  def invoice_params
+    params.require(:invoice).permit(
+      :invoice_number, :date, :due_date, :notes, :template, :invoice_type, :template_config,
+      from: [ :name, :email, :address ],
+      to: [ :name, :email, :address ],
+      items_attributes: [ :quantity, :price, :amount, :description ]
+  )
+  end
+end
