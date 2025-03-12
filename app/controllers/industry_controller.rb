0c498e42052aa796b0f25c8c25bd66c04c7e0025
heymanshe <himanshiksharma@outlook.com>
2025-03-12 17:04:53 +0530

    generate industry controller

---
+++ app/controllers/industry_controller.rb
@@ -0,0 +1,2 @@
+class IndustryController < ApplicationController
+end
---
+++ test/controllers/industry_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class IndustryControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
