b276981ba67929f2978c5f591da02a8642b8d200
heymanshe <himanshiksharma@outlook.com>
2025-03-12 13:02:08 +0530

    added schema model

---
+++ db/schema.rb
@@ -0,0 +1,70 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.0].define(version: 2025_03_11_053913) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
+  create_table "industries", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_industries_on_name", unique: true
+  end
+
+  create_table "invoices", force: :cascade do |t|
+    t.string "invoice_number", null: false
+    t.string "invoice_type", null: false
+    t.jsonb "from", default: {}, null: false
+    t.jsonb "to", default: {}, null: false
+    t.decimal "total_amount", precision: 10, scale: 2, default: "0.0"
+    t.string "template"
+    t.text "template_config"
+    t.date "due_date", null: false
+    t.date "date", null: false
+    t.text "notes"
+    t.jsonb "raw_data", default: {}
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["invoice_number"], name: "index_invoices_on_invoice_number", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.bigint "invoice_id", null: false
+    t.integer "quantity", null: false
+    t.decimal "price", precision: 10, scale: 2, null: false
+    t.decimal "amount", precision: 10, scale: 2, null: false
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["invoice_id"], name: "index_items_on_invoice_id"
+  end
+
+  create_table "templates", force: :cascade do |t|
+    t.string "name"
+    t.bigint "industry_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["industry_id"], name: "index_templates_on_industry_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "items", "invoices"
+  add_foreign_key "templates", "industries"
+end
